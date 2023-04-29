08d18bb33df7dffc56cf15ec6e8b78ee6ff1dcd7
Nesar Ahmed Naeem <hello@nesaran.com>
2023-04-29 15:30:31 +0600

    card updated

--- components/category/CategoryContainer.js
+++ components/category/CategoryContainer.js
@@ -46,7 +46,7 @@ const CategoryContainer = ({
         </div>
       ) : (
         <>
-          <div className="grid grid-cols-4 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-6 gap-4">
+          <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-6 gap-4">
             {brands.map((brand) => (
               <div className="max-w-xs rounded overflow-hidden shadow-lg">
                 <div className="bg-gray-400 h-40 flex justify-center items-center text-white text-4xl font-bold">
@@ -54,13 +54,22 @@ const CategoryContainer = ({
                 </div>
                 <div className="px-6 py-4">
                   <div className="font-bold text-xl mb-2 text-black dark:text-white">
-                    {brand.brandName}
+                    <h2
+                      className="lg:text-lg xl:text-lg text-sm font-bold mb-2 lg:w-48 xl:w-48 text-black dark:text-white"
+                      title={
+                        brand.brandName.length > 8 ? brand.brandName : null
+                      }
+                    >
+                      {brand.brandName.length > 8
+                        ? brand.brandName.slice(0, 8) + "..."
+                        : brand.brandName}
+                    </h2>
                   </div>
                   <Link
                     href={`/brand/${brand.brandName.toLowerCase()}`}
                     className="block text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
                   >
-                    Button
+                    View
                   </Link>
                 </div>
               </div>
--- pages/brands/[id].js
+++ pages/brands/[id].js
@@ -97,6 +97,29 @@ const Brands = ({ brandsData }) => {
                 </Link>
               </li>
 
+              <li>
+                <div className="flex items-center">
+                  <svg
+                    aria-hidden="true"
+                    className="w-6 h-6 text-gray-400"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                  <Link
+                    href={`/brands`}
+                    className="ml-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ml-2 dark:text-gray-400 dark:hover:text-white"
+                  >
+                    Brands
+                  </Link>
+                </div>
+              </li>
               <li aria-current="page">
                 <div className="flex items-center">
                   <svg
@@ -113,7 +136,7 @@ const Brands = ({ brandsData }) => {
                     ></path>
                   </svg>
                   <span className="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400">
-                    Brands
+                    Page {currentPage}
                   </span>
                 </div>
               </li>
